ce297078edc57a1dc7f859d54db9e2132e80c4fd
DoanJs <viethungtrieuphong1994@gmail.com>
2024-06-20 20:41:46 +0700

    JS_20_06_2024

--- client/src/utils/variable.ts
+++ client/src/utils/variable.ts
@@ -44,7 +44,6 @@ export const routeNavbar = {
       to: "/chuyenan",
       label: "Chuyên án",
     },
-
   ],
   routeRight: [
     {
@@ -52,275 +51,279 @@ export const routeNavbar = {
       label: "Nhập liệu",
     },
   ],
-}
-export const CODESECRET_REGISTER = "pa06-secret-@-hsnvntver04"
+};
+export const CODESECRET_REGISTER = "pa06-secret-@-hsnvntver04";
 export const IPADDRESS = "localhost";
 export const PORTSERVER = "5000";
 export const CSDLInput = [
   {
     cap: 1,
     to: "/nhaplieu/tinhtp",
-    label: "Tỉnh/TP"
+    label: "Tỉnh/TP",
+    data: [
+      { nameColumn: "TinhTP", type: "string" },
+      { nameColumn: "Cap", type: "string" },
+    ],
   },
   {
     cap: 1,
     to: "/nhaplieu/conganttpvatd",
-    label: "Công an Tỉnh/TP và tương đương"
+    label: "Công an Tỉnh/TP và tương đương",
   },
   {
     cap: 1,
     to: "/nhaplieu/conganqhvatd",
-    label: "Công an Quận/Huyện và tương đương"
+    label: "Công an Quận/Huyện và tương đương",
   },
   {
     cap: 1,
     to: "/nhaplieu/doi",
-    label: "Đội"
+    label: "Đội",
   },
   {
     cap: 1,
     to: "/nhaplieu/capbac",
-    label: "Cấp bậc"
+    label: "Cấp bậc",
   },
   {
     cap: 1,
     to: "/nhaplieu/chucvu",
-    label: "Chức vụ"
+    label: "Chức vụ",
   },
   {
     cap: 1,
     to: "/nhaplieu/quoctich",
-    label: "Quốc tịch"
+    label: "Quốc tịch",
   },
   {
     cap: 1,
     to: "/nhaplieu/dantoc",
-    label: "Dân tộc"
+    label: "Dân tộc",
   },
   {
     cap: 1,
     to: "/nhaplieu/tongiao",
-    label: "Tôn giáo"
+    label: "Tôn giáo",
   },
   {
     cap: 1,
     to: "/nhaplieu/ddnb",
-    label: "Đặc điểm nổi bật"
+    label: "Đặc điểm nổi bật",
   },
   {
     cap: 1,
     to: "/nhaplieu/hinhthuchd",
-    label: "Hình thức hoạt động"
+    label: "Hình thức hoạt động",
   },
   {
     cap: 1,
     to: "/nhaplieu/lldb",
-    label: "Lực lượng đặc biệt"
+    label: "Lực lượng đặc biệt",
   },
   {
     cap: 1,
     to: "/nhaplieu/loailldb",
-    label: "Loại lực lượng đặc biệt"
+    label: "Loại lực lượng đặc biệt",
   },
   {
     cap: 1,
     to: "/nhaplieu/loaidt",
-    label: "Loại đối tượng"
+    label: "Loại đối tượng",
   },
   {
     cap: 1,
     to: "/nhaplieu/tinhchatdt",
-    label: "Tính chất đối tượng"
-  },
-  {
-    cap: 1,
-    to: "/nhaplieu/cbcs",
-    label: "CBCS"
+    label: "Tính chất đối tượng",
   },
   {
     cap: 1,
     to: "/nhaplieu/bienphapdt",
-    label: "Biện pháp điều tra"
-  },
-  {
-    cap: 1,
-    to: "/nhaplieu/bienphapdtdt",
-    label: "Biện pháp điều tra/đối tượng"
+    label: "Biện pháp điều tra",
   },
   {
     cap: 1,
     to: "/nhaplieu/capca",
-    label: "Cấp công an"
+    label: "Cấp công an",
   },
   {
     cap: 2,
-    to: '/nhaplieu/doituong',
-    label: 'Đối tượng'
+    to: "/nhaplieu/cbcs",
+    label: "CBCS",
+  },
+  {
+    cap: 2,
+    to: "/nhaplieu/doituong",
+    label: "Đối tượng",
+  },
+  {
+    cap: 2,
+    to: "/nhaplieu/bienphapdtdt",
+    label: "Biện pháp điều tra/đối tượng",
   },
   {
     cap: 3,
-    to: '/nhaplieu/denghitsnt',
-    label: 'Đề nghị TSNT'
+    to: "/nhaplieu/denghitsnt",
+    label: "Đề nghị TSNT",
   },
   {
     cap: 3,
-    to: '/nhaplieu/daumoiphdn',
-    label: 'Đầu mối phối hợp/đề nghị'
+    to: "/nhaplieu/daumoiphdn",
+    label: "Đầu mối phối hợp/đề nghị",
   },
   {
     cap: 3,
-    to: '/nhaplieu/kyduyetdn',
-    label: 'Ký duyệt đề nghị'
+    to: "/nhaplieu/kyduyetdn",
+    label: "Ký duyệt đề nghị",
   },
   {
     cap: 3,
-    to: '/nhaplieu/denghitsnttinhtp',
-    label: 'Đề nghị TSNT/Tỉnh TP'
+    to: "/nhaplieu/denghitsnttinhtp",
+    label: "Đề nghị TSNT/Tỉnh TP",
   },
   {
     cap: 4,
-    to: '/nhaplieu/quyetdinhtsnt',
-    label: 'Quyết định TSNT'
+    to: "/nhaplieu/quyetdinhtsnt",
+    label: "Quyết định TSNT",
   },
   {
     cap: 4,
-    to: '/nhaplieu/quyetdinhtsnttinhtp',
-    label: 'Quyết định TSNT/Tỉnh TP'
+    to: "/nhaplieu/quyetdinhtsnttinhtp",
+    label: "Quyết định TSNT/Tỉnh TP",
   },
   {
     cap: 5,
-    to: '/nhaplieu/kehoachtsnt',
-    label: 'Kế hoạch TSNT'
+    to: "/nhaplieu/kehoachtsnt",
+    label: "Kế hoạch TSNT",
   },
   {
     cap: 5,
-    to: '/nhaplieu/kehoachtsntlldb',
-    label: 'Kế hoạch TSNT/LLDB'
+    to: "/nhaplieu/kehoachtsntlldb",
+    label: "Kế hoạch TSNT/LLDB",
   },
   {
     cap: 5,
-    to: '/nhaplieu/lucluongthamgiakh',
-    label: 'Lực lượng tham gia KH'
+    to: "/nhaplieu/lucluongthamgiakh",
+    label: "Lực lượng tham gia KH",
   },
   {
     cap: 5,
-    to: '/nhaplieu/tramct',
-    label: 'Trạm công tác'
+    to: "/nhaplieu/tramct",
+    label: "Trạm công tác",
   },
   {
     cap: 6,
-    to: '/nhaplieu/bienbanrkn',
-    label: 'Biên bản RKN'
+    to: "/nhaplieu/bienbanrkn",
+    label: "Biên bản RKN",
   },
   {
     cap: 6,
-    to: '/nhaplieu/bienbanrknldtg',
-    label: 'Biên bản RKN/Lãnh đạo tham gia'
+    to: "/nhaplieu/bienbanrknldtg",
+    label: "Biên bản RKN/Lãnh đạo tham gia",
   },
   {
     cap: 6,
-    to: '/nhaplieu/baocaoktdn',
-    label: 'Báo cáo kết thúc đề nghị'
+    to: "/nhaplieu/baocaoktdn",
+    label: "Báo cáo kết thúc đề nghị",
   },
   {
     cap: 6,
-    to: '/nhaplieu/baocaokqgh',
-    label: 'Báo cáo kết quả ghi hình'
+    to: "/nhaplieu/baocaokqgh",
+    label: "Báo cáo kết quả ghi hình",
   },
   {
     cap: 6,
-    to: '/nhaplieu/baocaokqghcbcs',
-    label: 'Báo cáo kết quả ghi hình/CBCS'
+    to: "/nhaplieu/baocaokqghcbcs",
+    label: "Báo cáo kết quả ghi hình/CBCS",
   },
   {
     cap: 6,
-    to: '/nhaplieu/baocaokqxmdiachi',
-    label: 'Báo cáo kết quả xác minh địa chỉ'
+    to: "/nhaplieu/baocaokqxmdiachi",
+    label: "Báo cáo kết quả xác minh địa chỉ",
   },
   {
     cap: 6,
-    to: '/nhaplieu/baocaokqxmquanhe',
-    label: 'Báo cáo kết quả xác minh quan hệ'
+    to: "/nhaplieu/baocaokqxmquanhe",
+    label: "Báo cáo kết quả xác minh quan hệ",
   },
   {
     cap: 6,
-    to: '/nhaplieu/baocaophqh',
-    label: 'Báo cáo phát hiện quan hệ'
+    to: "/nhaplieu/baocaophqh",
+    label: "Báo cáo phát hiện quan hệ",
   },
   {
     cap: 6,
-    to: '/nhaplieu/baocaophqhcbcs',
-    label: 'Báo cáo phát hiện quan hệ/CBCS'
+    to: "/nhaplieu/baocaophqhcbcs",
+    label: "Báo cáo phát hiện quan hệ/CBCS",
   },
   {
     cap: 6,
-    to: '/nhaplieu/danhgiatsth',
-    label: 'Đánh giá trinh sát thực hiện'
+    to: "/nhaplieu/danhgiatsth",
+    label: "Đánh giá trinh sát thực hiện",
   },
   {
     cap: 6,
-    to: '/nhaplieu/diachinv',
-    label: 'Địa chỉ nghi vấn'
+    to: "/nhaplieu/diachinv",
+    label: "Địa chỉ nghi vấn",
   },
   {
     cap: 6,
-    to: '/nhaplieu/diachinvcbcs',
-    label: 'Địa chỉ nghi vấn/CBCS'
+    to: "/nhaplieu/diachinvcbcs",
+    label: "Địa chỉ nghi vấn/CBCS",
   },
   {
     cap: 6,
-    to: '/nhaplieu/ketquatsnt',
-    label: 'Kết quả TSNT'
+    to: "/nhaplieu/ketquatsnt",
+    label: "Kết quả TSNT",
   },
   {
     cap: 6,
-    to: '/nhaplieu/ketquatsntddnb',
-    label: 'Kết quả TSNT/DDNB'
+    to: "/nhaplieu/ketquatsntddnb",
+    label: "Kết quả TSNT/DDNB",
   },
   {
     cap: 6,
-    to: '/nhaplieu/ketquatsnttinhtp',
-    label: 'Kết quả TSNT/Tỉnh TP'
+    to: "/nhaplieu/ketquatsnttinhtp",
+    label: "Kết quả TSNT/Tỉnh TP",
   },
   {
     cap: 6,
-    to: '/nhaplieu/ketquaxmdiachi',
-    label: 'Kết quả xác minh địa chỉ'
+    to: "/nhaplieu/ketquaxmdiachi",
+    label: "Kết quả xác minh địa chỉ",
   },
   {
     cap: 6,
-    to: '/nhaplieu/ketquaxmquanhe',
-    label: 'Kết quả xác minh quan hệ'
+    to: "/nhaplieu/ketquaxmquanhe",
+    label: "Kết quả xác minh quan hệ",
   },
   {
     cap: 6,
-    to: '/nhaplieu/phuongtiennv',
-    label: 'Phương tiện nghiệp vụ'
+    to: "/nhaplieu/phuongtiennv",
+    label: "Phương tiện nghiệp vụ",
   },
   {
     cap: 7,
-    to: '/nhaplieu/chuyenan',
-    label: 'Chuyên án'
+    to: "/nhaplieu/chuyenan",
+    label: "Chuyên án",
   },
   {
     cap: 7,
-    to: '/nhaplieu/doituongca',
-    label: 'Đối tượng chuyên án'
+    to: "/nhaplieu/doituongca",
+    label: "Đối tượng chuyên án",
   },
-]
+];
 export const keyValueSame_TenDT = [
   "DanhGiaTSTH",
   "TSThucHien_BaoCaoPHQHs-TenDT",
   "TSThucHien_BaoCaoKQGHs-TenDT",
   "TSThucHien_PhuongTienNVs-TenDT",
-  "TSThucHien_DiaChiNVs-TenDT"
-]
+  "TSThucHien_DiaChiNVs-TenDT",
+];
 export const keyValueSame_BiDanh_YC = [
   "TSThucHien_BaoCaoPHQHs-BiDanhYC",
   "TSThucHien_BaoCaoKQGHs-BiDanhYC",
   "TSThucHien_PhuongTienNVs-BiDanhYC",
-  "TSThucHien_DiaChiNVs-BiDanhYC"
-]
+  "TSThucHien_DiaChiNVs-BiDanhYC",
+];
 
 export const arr_server = [
   {
@@ -345,12 +348,7 @@ export const arr_server = [
   },
 ];
 
-
-
-
-
-
-
-
-
-export const keyValueSame_DanhGiaTSTH_BiDanhYC = ["TSThucHien_BaoCaoPHQHs-BiDanhYC", "TSThucHien_BaoCaoKQGHs-BiDanhYC"]
+export const keyValueSame_DanhGiaTSTH_BiDanhYC = [
+  "TSThucHien_BaoCaoPHQHs-BiDanhYC",
+  "TSThucHien_BaoCaoKQGHs-BiDanhYC",
+];
